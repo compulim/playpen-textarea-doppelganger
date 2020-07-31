0e946a3c66f22bcb85e9ffb78c742eb18faa4d60
William Wong <compulim@hotmail.com>
2020-07-31 07:33:43 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './App.css';
+import AutoResizeTextArea from './AutoResizeTextArea';
+
+export default function App() {
+  return (
+    <div className="app">
+      <button className="app__button">
+        <span role="img" aria-label="upload">
+          📁
+        </span>
+      </button>
+      <AutoResizeTextArea className="app__text-box" />
+      <button className="app__button">
+        <span role="img" aria-label="send">
+          📩
+        </span>
+      </button>
+    </div>
+  );
+}
---
+++ src/AutoResizeTextArea.js
@@ -0,0 +1,21 @@
+import classNames from 'classnames';
+import React, { useCallback, useState } from 'react';
+
+import './AutoResizeTextArea.css';
+
+function App({ className }) {
+  const [value, setValue] = useState(
+    'Est minim tempor nulla officia nisi ex officia incididunt veniam do cillum tempor. Magna pariatur consectetur in non nisi dolor culpa deserunt sint proident fugiat reprehenderit. Commodo amet sunt aliquip nisi anim anim eu velit qui anim adipisicing quis ex. Nisi tempor velit magna mollit cillum culpa.'
+  );
+  const handleTextAreaChange = useCallback(({ target: { value } }) => setValue(value), [setValue]);
+
+  return (
+    <div className={classNames('auto-resize-textarea', className)}>
+      {/* We need to add a space here, so blank lines will be counted in the doppelganger. */}
+      <div className="auto-resize-textarea__doppelganger">{value}&nbsp;</div>
+      <textarea className="auto-resize-textarea__textarea" onChange={handleTextAreaChange} value={value} />
+    </div>
+  );
+}
+
+export default App;
